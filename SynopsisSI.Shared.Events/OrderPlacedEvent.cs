3b921fd42a0ec5f6d39842bd5d0a78b3f3aeed5c
Klaus Hviid <klahvi01@easv365.dk>
2025-05-22 14:48:41 +0200

    Shared events - beginning. Skeleton for shared infrastructure.

---
+++ SynopsisSI.Shared.Events/OrderPlacedEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace SynopsisSI.Shared.Events;
+
+public class OrderPlacedEvent
+{
+    //Published by OrderService when an order is placed
+}
---
+++ SynopsisSI.Shared.Infrastructure/UseWithCaution.cs
@@ -0,0 +1,6 @@
+﻿namespace SynopsisSI.Shared.Infrastructure;
+
+public class UseWithCaution
+{
+    //Before implemeting shared message brokers, infrastucture, etc. check if really needed for this scope
+}
