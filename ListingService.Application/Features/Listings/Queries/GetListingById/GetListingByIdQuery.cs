b6d430c11e73d475bb303b9cd2b107c708948a84
Klaus Hviid <klahvi01@easv365.dk>
2025-05-23 20:22:51 +0200

    Moved folders around - for fuck sake what a clusterfuck

--- ListingService.Application/Features/Listings/Queries/GetListingById/GetListingByIdQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿// using MediatR;
-
-namespace SynopsisSI.Services.ListingService.Application.Features.Listings.Queries.GetListingById;
-
-public class GetListingByIdQuery // : IRequest<ListingItemDto?>
-{
-    public string Id { get; set; } = string.Empty;
-}
--- SynopsisSI.Shared.Infrastructure/UseWithCaution.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SynopsisSI.Shared.Infrastructure;
-
-public class UseWithCaution
-{
-    //Before implemeting shared message brokers, infrastucture, etc. check if really needed for this scope
-}
